936de729cc7a6340afb0e27de7536407753781a5
shawticus <shawmakesmagic@gmail.com>
2021-03-19 03:11:22 -0700

    Aframe works

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,8 +9,8 @@ function App() {
       <Nav />
       <FileUpload />
       <Scene
+        environment={{ preset: "forest" }}
         vr-mode-ui='enabled: false'
-        embedded
         arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
           <Entity
             primitive="a-camera"
