f3aaf36d9631388f9137c74ce63f221b5997e6ca
shawticus <shawmakesmagic@gmail.com>
2021-03-19 03:07:01 -0700

    embed aframe

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,18 +9,8 @@ function App() {
       <Nav />
       <FileUpload />
       <Scene
-      environment={{
-          preset: 'contact',
-          seed: 2,
-          lightPosition: { x: 0.0, y: 0.03, z: -0.5 },
-          fog: 0.8,
-          ground: 'none',
-          groundYScale: 6.31,
-          groundTexture: 'walkernoise',
-          groundColor: '#8a7f8a',
-          grid: 'none'
-        }}
         vr-mode-ui='enabled: false'
+        embedded
         arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
           <Entity
             primitive="a-camera"
