690744eed1266c283fad7c40ec30704f17b79526
shawticus <shawmakesmagic@gmail.com>
2021-03-19 02:03:34 -0700

    Basic hardcoded initial commit, react client and express server

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import FileUpload from "./components/FileUpload.js"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Cache
+      </header>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+var cors = require('cors')
+app.use(cors({ origin: "*"}))
+
+
+
+app.use(fileUpload());
+app.use(express.static('public'))
+
+// Upload Endpoint
+app.post('/upload', (req, res) => {
+    console.log("Received upload request")
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/public/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
+
+app.listen(5000, () => console.log('Server Started...'));
